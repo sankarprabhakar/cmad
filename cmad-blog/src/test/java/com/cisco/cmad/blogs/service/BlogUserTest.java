00d2bf45017a38729e53df351ce2805fa00d8f65
Harsha Nanjundaiaha <harsha.tumkur@gmail.com>
2017-05-09 10:26:46 +0530

    changed persistance to refer mysql docker

--- cmad-blog/src/test/java/com/cisco/cmad/blogs/service/BlogUserTest.java
+++ cmad-blog/src/test/java/com/cisco/cmad/blogs/service/BlogUserTest.java
@@ -1,34 +1,42 @@
 package com.cisco.cmad.blogs.service;
 
+import static org.junit.Assert.fail;
+
 import org.junit.Test;
+
+import com.cisco.cmad.blogs.api.DuplicateEntityException;
+import com.cisco.cmad.blogs.api.EntityException;
+import com.cisco.cmad.blogs.api.InvalidEntityException;
+import com.cisco.cmad.blogs.api.User;
+import com.cisco.cmad.blogs.api.Users;
 
 public class BlogUserTest {
 
     @Test
     public void createUser() {
-        assert (true);
-        // Users userService = UsersService.getInstance();
-        // User admin = new User();
-        // admin.setUserId("admin");
-        // admin.setEmailId("admin@tecblog.com");
-        // admin.setFirstName("admin");
-        // admin.setPassword("xyz");
-        // admin.setLastName("");
-        //
-        // try {
-        // userService.create(admin);
-        // User createdUser = userService.read("admin");
-        // assert (createdUser.getUserId() == "admin");
-        // } catch (InvalidEntityException ibe) {
-        // fail();
-        // } catch (DuplicateEntityException dbe) {
-        // fail();
-        // } catch (EntityException le) {
-        // fail();
-        // } catch (Exception e) {
-        // e.printStackTrace();
-        // fail();
-        // }
+        // assert (true);
+        Users userService = UsersService.getInstance();
+        User admin = new User();
+        admin.setUserId("admin");
+        admin.setEmailId("admin@tecblog.com");
+        admin.setFirstName("admin");
+        admin.setPassword("xyz");
+        admin.setLastName("");
+
+        try {
+            userService.create(admin);
+            User createdUser = userService.read("admin");
+            assert (createdUser.getUserId() == "admin");
+        } catch (InvalidEntityException ibe) {
+            fail();
+        } catch (DuplicateEntityException dbe) {
+            fail();
+        } catch (EntityException le) {
+            fail();
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
     }
 
 }
