aae081c07298d588a1fc9994ef104c78dbb0918a
soumya suansia <soumya.suansia@gmail.com>
2017-05-14 19:08:54 +0530

    Added test CRUD test cases for blog app

--- cmad-blog/src/test/java/com/cisco/cmad/blogs/service/BlogUserTest.java
+++ cmad-blog/src/test/java/com/cisco/cmad/blogs/service/BlogUserTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
+import com.cisco.cmad.blogs.api.DataNotFoundException;
 import com.cisco.cmad.blogs.api.DuplicateEntityException;
 import com.cisco.cmad.blogs.api.EntityException;
 import com.cisco.cmad.blogs.api.InvalidEntityException;
@@ -12,11 +13,12 @@ import com.cisco.cmad.blogs.api.Users;
 
 public class BlogUserTest {
 
-    @Test
+	// assert (true);
+	Users userService = UsersService.getInstance();
+	User admin = new User();
+
+	@Test
     public void createUser() {
-        // assert (true);
-        Users userService = UsersService.getInstance();
-        User admin = new User();
         admin.setUserId("admin");
         admin.setEmailId("admin@tecblog.com");
         admin.setFirstName("admin");
@@ -27,11 +29,75 @@ public class BlogUserTest {
             userService.create(admin);
             User createdUser = userService.read("admin");
             assert (createdUser.getUserId() == "admin");
+            assert (createdUser.getEmailId() == "admin@tecblog.com");
+            assert (createdUser.getFirstName() == "admin");
+            assert (createdUser.getLastName() == "");
         } catch (InvalidEntityException ibe) {
             fail();
         } catch (DuplicateEntityException dbe) {
             fail();
         } catch (EntityException le) {
+            fail();
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+	@Test
+    public void authenticateUser() {
+        try {
+            userService.authenticate("admin", "xyz");
+        } catch (SecurityException se) {
+            fail();
+        } catch (EntityException  ee) {
+            fail();
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+	@Test
+    public void updateUser() {
+
+        admin.setEmailId("testadmin@tecblog.com");
+        admin.setFirstName("testadmin");
+
+        try {
+            userService.update(admin);
+            User createdUser = userService.read("admin");
+            
+            assert (createdUser.getUserId() == "admin");
+            assert (createdUser.getEmailId() == "testadmin@tecblog.com");
+            assert (createdUser.getFirstName() == "testadmin");
+            
+        } catch (InvalidEntityException ibe) {
+            fail();
+        } catch (DuplicateEntityException dbe) {
+            fail();
+        } catch (EntityException le) {
+            fail();
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+	
+	@Test
+    public void deleteUser() {
+
+        try {
+            userService.delete("admin");
+            User createdUser = userService.read("admin");
+            
+            assert (createdUser.getUserId() == "admin");
+            assert (createdUser.getEmailId() == "testadmin@tecblog.com");
+            assert (createdUser.getFirstName() == "testadmin");
+            
+        } catch (DataNotFoundException ibe) {
+            fail();
+        } catch (EntityException dbe) {
             fail();
         } catch (Exception e) {
             e.printStackTrace();
