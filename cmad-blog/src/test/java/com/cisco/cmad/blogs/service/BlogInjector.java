f8f3dc2a9471bc3e7b5d3d920aa740d54739914e
Harsha Nanjundaiaha <harsha.tumkur@gmail.com>
2017-05-25 18:12:06 +0530

    blogs injector is working

--- cmad-blog/src/test/java/com/cisco/cmad/blogs/service/BlogInjector.java
+++ cmad-blog/src/test/java/com/cisco/cmad/blogs/service/BlogInjector.java
@@ -19,10 +19,12 @@ import org.json.simple.parser.ParseException;
 
 import com.cisco.cmad.blogs.api.Blog;
 import com.cisco.cmad.blogs.api.Comment;
+import com.cisco.cmad.blogs.api.DataNotFoundException;
 import com.cisco.cmad.blogs.api.DuplicateEntityException;
 import com.cisco.cmad.blogs.api.EntityException;
 import com.cisco.cmad.blogs.api.InvalidEntityException;
 import com.cisco.cmad.blogs.api.User;
+import com.cisco.cmad.blogs.common.config.AppConfig;
 
 public class BlogInjector {
     // Instantiate UserService, BlogService, CommentService
@@ -45,7 +47,6 @@ public class BlogInjector {
 
     // create user [This is helpfull for import]
     static void createUser(String userId, String email, String firstName, String LastName, String passWord) {
-
         User user = new User();
         user.setUserId(userId).setEmailId(email).setFirstName(firstName).setLastName(LastName).setPassword(passWord);
         userService.create(user);
@@ -69,12 +70,31 @@ public class BlogInjector {
     static void createBlog(User blogger, String category, String title, String content) {
         try {
             Blog blog = new Blog(); /// setBlogId(1)
-            blog.setAuthor(blogger).setBlogText(title).setTitle(content).setCategory(category);
+            blog.setAuthor(blogger).setBlogText(content).setTitle(title).setCategory(category);
             blogService.create(blog);
         } catch (Exception e) {
             e.printStackTrace();
 
         }
+    }
+
+    // get all bloglist
+    static List<Blog> getAllBlogs(int size) {
+        List<Blog> blogs = new ArrayList<Blog>();
+        try {
+            int pageNum = 0;
+            int lastPage = size / AppConfig.MAX_PAGE_SIZE;
+            while (pageNum <= lastPage) {
+                List<Blog> pageBlogs = blogService.readAllBlogs(pageNum);
+                blogs.addAll(pageBlogs);
+                pageNum++;
+            }
+        } catch (DataNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return blogs;
     }
 
     // delete a blog
@@ -140,7 +160,7 @@ public class BlogInjector {
     }
 
     // create comment
-    void createCommentToBlog(User blogger, Blog blogPost, String commentMsg) {
+    static void createCommentToBlog(User blogger, Blog blogPost, String commentMsg) {
         try {
             // create comment object
             Comment comment = new Comment();
@@ -165,19 +185,19 @@ public class BlogInjector {
 
     // Main function
     public static void main(String[] args) {
-        int NUM_OF_USER, NUM_OF_BLOG, NUM_OF_COMMENTS;
+        int NUM_OF_USERS, NUM_OF_BLOGS, NUM_OF_COMMENTS;
 
         BlogInjector bobj = new BlogInjector();
         bobj.readJson();
 
         // get total count of each element
-        NUM_OF_USER = bobj.users.size();
-        NUM_OF_BLOG = bobj.blogs.size();
+        NUM_OF_USERS = bobj.users.size();
+        NUM_OF_BLOGS = bobj.blogs.size();
         NUM_OF_COMMENTS = bobj.comments.size();
 
         // create multiple users
-        for (int c = 0; c < NUM_OF_USER; c++) {
-            User usr = bobj.users.get(c);
+        for (int index = 0; index < NUM_OF_USERS; index++) {
+            User usr = bobj.users.get(index);
             BlogInjector.createUser(usr.getUserId(), usr.getEmailId(), usr.getFirstName(), usr.getLastName(),
                     usr.getPassword());
         }
@@ -186,30 +206,25 @@ public class BlogInjector {
         List<User> usersList = BlogInjector.fetchUser();
 
         // create multiple blogs
-        for (int c = 0; c < NUM_OF_BLOG; c++) {
-            Blog blog = bobj.blogs.get(c);
-            int userIndex = (c / NUM_OF_USER) % NUM_OF_USER;
-            blog.setAuthor(usersList.get(userIndex));
-            System.out.println(usersList.get(userIndex).getFirstName());
-            BlogInjector.blogService.create(blog);
+        for (int index = 0; index < NUM_OF_BLOGS; index++) {
+            Blog blog = bobj.blogs.get(index);
+            int userIndex = index % NUM_OF_USERS;
+            User blogger = usersList.get(userIndex);
+            System.out.println(blogger.getFirstName());
+            // BlogInjector.blogService.create(blog);
+            BlogInjector.createBlog(blogger, blog.getCategory(), blog.getTitle(), blog.getBlogText());
         }
 
+        List<Blog> blogs = BlogInjector.getAllBlogs(NUM_OF_BLOGS);
         // create multiple comments
-        for (int c = 0; c < NUM_OF_BLOG; c++) { // loop through blogs
-            Blog blog = bobj.blogs.get(c);
+        for (int index = 0; index < NUM_OF_BLOGS; index++) {
+            Blog blog = blogs.get(index);
             // run through comments loop
-            int c1 = 0;
-            while (c1 < (NUM_OF_BLOG % NUM_OF_COMMENTS)) {
-                Comment comment = bobj.comments.get(c1);
-                comment.setBlog(blog);
-
-                // select user index
-                comment.setAddedBy(usersList.get(c1 % NUM_OF_USER));
-
-                // post a comment
-                BlogInjector.commentService.create(comment);
-                // increment counter for comment
-                c1++;
+            int commentIndex = 0;
+            while (commentIndex < NUM_OF_COMMENTS) {
+                Comment comment = bobj.comments.get(commentIndex);
+                BlogInjector.createCommentToBlog(blog.getAuthor(), blog, comment.getCommentText());
+                commentIndex++;
             }
         }
     }
